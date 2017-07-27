5af509d0e94fa970c5e03fb545f51c56af4a5329
palakagarwal2016 <thepalakagarwal@gmail.com>
2017-07-26 17:01:35 -0700

    made files and stuff

--- App.js
+++ App.js
@@ -1,22 +1,109 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  AsyncStorage,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button,
+  RefreshControl
+} from 'react-native';
 import Login from './components/login'
+import Register from './components/register'
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class Home extends React.Component {
+  static navigationOptions = {
+    title: 'Home'
+  };
+
+  login() {
+    this.props.navigation.navigate('Login');
+  }
+  register() {
+    this.props.navigation.navigate('Register');
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Login />
+        <Text style={styles.textBig}>Login to EasyForm!</Text>
+        <TouchableOpacity onPress={ () => {this.login()} } style={[styles.button, styles.buttonGreen]}>
+          <Text style={styles.buttonLabel}>Tap to Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+          <Text style={styles.buttonLabel}>Tap to Register</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+export default StackNavigator({
+  Home: {
+    screen: Home,
+  },
+  Login: {
+    screen: Login,
+  },
+  Register: {
+    screen: Register,
+  },
+}, {initialRouteName: 'Home'});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  containerFull: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  textBig: {
+    fontSize: 36,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5
+  },
+  buttonRed: {
+    backgroundColor: '#FF585B',
+  },
+  buttonBlue: {
+    backgroundColor: '#0074D9',
+  },
+  buttonGreen: {
+    backgroundColor: '#2ECC40'
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white'
+  }
 });
